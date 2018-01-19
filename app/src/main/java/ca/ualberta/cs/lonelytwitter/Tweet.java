dbca5d510a79faed5932385ec2c644570e3e07e2
Punam Woosaree <PunamWoosaree@Punams-MacBook-Air.local>
2018-01-18 19:16:39 -0700

    List of moods

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
@@ -1,6 +1,7 @@
 package ca.ualberta.cs.lonelytwitter;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by PunamWoosaree on 2018-01-18.
@@ -10,6 +11,7 @@ public abstract class Tweet implements Tweetable {
 
     private String message;
     private Date date;
+    private List<currentMood> moods;
 
     public Tweet(String message) {
         this.message = message;
@@ -43,4 +45,8 @@ public abstract class Tweet implements Tweetable {
     }
 
     public abstract Boolean isImportant();
+    
+    public void add_mood(currentMood){
+        this.moods.add(mood);
+    }
 }
