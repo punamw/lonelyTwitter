37f2e854959789bc0948344f9e8d00496f91765a
Punam Woosaree <PunamWoosaree@Punams-MacBook-Air.local>
2018-02-16 13:03:02 -0700

    AddTweetTest

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
@@ -70,6 +70,22 @@ public class TweetListTest extends ActivityInstrumentationTestCase2 {
         assertEquals(tweetSize, 1);
     }
 
+    public void testAddTweet(){
+        Throwable ex;
+        TweetList tweets = new TweetList();
+        Tweet tweet = new NormalTweet("adding a normal Tweet");
+        Tweet tweet2 = new NormalTweet("adding a normal Tweet");
+        tweets.add(tweet);
+        assertTrue(tweets.hasTweet(tweet));
+        //Source: https://stackoverflow.com/questions/156503/how-do-you-assert-that-a-certain-exception-is-thrown-in-junit-4-tests
+        //Accessed on February 15, 2018
+        try{
+            tweets.add(tweet2);
+        } catch (IllegalArgumentException e){
+            ex = e;
+            assertTrue(ex instanceof IllegalArgumentException);
+        }
+    }
 
 
 }
